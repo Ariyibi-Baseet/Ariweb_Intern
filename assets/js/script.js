39b62c3f5f5e2a24427d0ebbeaf0a967ceddbfa6
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2022-08-21 09:43:45 +0100

    fix overflow when preloader is on

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 function offPreloader()
 {
     document.getElementById('preloader').classList.add('offPreloader');
+    document.body.classList.add('removeOverflow');
 }
 
 setTimeout(offPreloader,5000);
