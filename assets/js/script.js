dcbeed3ab21eb1954b001222082848aa1f68a63a
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2022-08-26 13:11:17 +0100

    implement countdown for the site launch

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,13 @@ const toDate = new Date("September 5 2022 00:00:00");
 
 
 // FUNCTION TO SHOW PRELOADER FOR 5 SECONDS
-function offPreloader()
-{
-    document.getElementById('preloader').classList.add('offPreloader');
-    document.body.classList.add('removeOverflow');
-}
+// function offPreloader()
+// {
+//     document.getElementById('preloader').classList.add('offPreloader');
+//     document.body.classList.add('removeOverflow');
+// }
 
-setTimeout(offPreloader,5000);
+// setTimeout(offPreloader,5000);
 
 
 // COUNTDOWN TIMER
@@ -24,11 +24,18 @@ function countDownTimer()
     const currentTime = new Date();
     const timeDifference = toDate - currentTime;
 
-    const days = Math.floor(timeDifference / 1000 / 60 / 60 / 24);
-    const hours = Math.floor(timeDifference / 1000 / 60 / 60)%24;
-    console.log(hours);
+    const days_ = Math.floor(timeDifference / 1000 / 60 / 60 / 24);
+    const hours_ = Math.floor(timeDifference / 1000 / 60 / 60) % 24;
+    const minutes_ = Math.floor(timeDifference / 1000 / 60) % 60;
+    const seconds_ = Math.floor(timeDifference / 1000) % 60;
+
+    days.innerHTML = days_;
+    hours.innerHTML = hours_ < 10 ? '0' + hours_ : hours_;
+    minutes.innerHTML = minutes_ < 10 ? '0' + minutes_ : minutes_;
+    seconds.innerHTML = seconds_ < 10 ? '0' + seconds_ : seconds_;
+    // console.log(seconds);
 }
-countDownTimer();
+setInterval(countDownTimer,1000);
 
 
 
